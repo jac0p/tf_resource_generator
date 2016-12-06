6296d729f1b441cf88cf30b7c7fe8c1809d8d8b0
rolandvarga <rvarga@starschema.net>
2016-12-06 17:33:10 -0500

    least important functions added

--- tf_generator.js
+++ tf_generator.js
@@ -1,8 +1,7 @@
 // imports
 const commandLineArgs = require('command-line-args');
-const getUsage = require('command-line-usage')
-const header = require('./ascii-header')
-const ansi = require('ansi-escape-sequences')
+const getUsage = require('command-line-usage');
+const fs = require('fs');
 
 
 // CLI fun
@@ -29,10 +28,42 @@ const sections = [
 // CSV "header"
 const headers = ["hostname", "cpu", "memory", "disk", "network_adapter", "ip"];
 
+function createPostDestroy(properties) {
+  var stream = fs.createWriteStream("out/post-destroy");
+
+  stream.once('open', function(fd) {
+    stream.write("#!/bin/bash\n\n");
+    for (i = 0; i < properties.length; i++) { 
+      var hostname = properties[i]["hostname"];
+      var ip = properties[i]["ip"];
+      console.log(`Adding machine: ${hostname}, with IP: ${ip}`);
+      stream.write("ssh-keygen -f ~/.ssh/known_hosts -R " + hostname + "\n");
+      stream.write("ssh-keygen -f ~/.ssh/known_hosts -R " + ip + "\n");
+    }
+    stream.end();
+  });
+}
+
+
+function assocMachine(properties) {
+  o = {};
+  for (i = 0; i < headers.length; i++) { o[headers[i]] = properties[i]; }
+  return o;
+}
+
 
 function parseConfig(config) {
-  return config;
+  var machines = [];
+  var data = fs.readFileSync(config);
+  var lines = data.toString().split('\n');
 
+  for (i = 0; i < (lines.length - 1); i++) {
+    var properties = lines[i].toString().split(',');
+    machines.push(properties)
+  }
+
+  machines = machines.map(assocMachine);
+  return machines;
 }
 
 
@@ -40,9 +71,13 @@ function main() {
   if (options["config"] == undefined || options["help"] == true) {
     console.log(getUsage(sections));
   } else {
-    var machines = parseConfig(options["config"]);
-    console.log(machines)
+    console.log("Parsing configuration.")
+    var machineProperties = parseConfig(options["config"]);
+    console.log("Creating post-destroy file.");
+    createPostDestroy(machineProperties);
   } 
 }
 
 main();
+
+
